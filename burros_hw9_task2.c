43636bb93bd3f0d8f885059947e40d12767c3a8d
Sterling Woodruff <sterlingwoodruff@mail.weber.edu>
2017-04-05 01:16:02 +0000

    Initial Commit of Everything

---
+++ burros_hw9_task2.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  burros_hw9_task2.c
+ *       	Usage:  ./burros_hw9_task2.c
+ *    Description:  hw9_task2 
+ *
+ *        Version:  1.0
+ *        Created:  04/05/2017 01:14:43 AM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Sterling, Sherry
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+/* Function Prototypes */
+
+/* Main Program */
+int main (int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
