c1b090c861a9b6ddde1e8c425f7b6190b59d2204
Sherry Masutani <sherrymasutani@mail.weber.edu>
2017-04-11 02:45:56 +0000

    final edits

--- burros_hw9_task2.c
+++ burros_hw9_task2.c
@@ -15,47 +15,51 @@
 #include <stdio.h>		/* For Standard I/O */
 #include <stdlib.h>
 
-/* Function Prototypes */
-
 /* Main Program */
 int main (int argc, char *argv[])
 {
+//Open file
 	FILE* zone1 = fopen("zone1.txt", "r");
 
+//File error messgae
 	if (zone1 == NULL)
 	{
 		printf("The file was not successfully opened.\n");
 		exit(1);
 	}
+
 //Set variables
 	int counter = 0;
 	float x = 0.0, sumx = 0.0;
 	float y = 0.0, sumy = 0.0;
 	float sumxy = 0.0;
 	float sumx2 = 0.0;
+	float m, b;
+	int zone[9];
 
 //Save variables from file
-	while (!feof(zone1))
-	{
-		zone1[counter] = x;
+	while (fscanf(zone1, "%d", &zone[counter]) != EOF)
+	{  
+	if (counter % 2 == 0 || counter == 0)
+		{
+		x = zone[counter];
 		sumx += x;
 		sumx2 += x*x;
 		counter++;
-
-		zone1[counter] = y;
+		}
+	else
+		{
+		y = zone[counter];
 		sumy += y;
 		sumxy += x*y;
 		counter++;
+		}
 	}
-//Calculate values
 
-	float m, b;
-	m = (x*y - 4.0*sumxy)/(x*x - 4.0*sumx2);
-	b = (x*sumxy - sumx2*y)/(x*x - 4.0*sumx2);
-	printf("m = %1.3f	b = %1.3f", m, b);
+//Calculate values
+	m = (sumx*sumy - 4.0*sumxy)/(sumx*sumx - 4.0*sumx2);
+	b = (sumx*sumxy - sumx2*sumy)/(sumx*sumx - 4.0*sumx2);
+	printf("m = %1.3f	b = %1.3f\n", m, b);
 
 	return 0;
 }
-/* Function Defenitions */
-
-
